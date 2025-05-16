0bb54220800ac69d440c32562807a7515b4963e0
Pearson <pearsontht@gmail.com>
2025-05-16 13:26:16 -0400

    V.0.1.0
    Added base code

--- main.py
+++ main.py
@@ -1,1 +1,85 @@
 # Main game loop goes here
+
+import arcade
+
+# Constants
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
+WINDOW_TITLE = "PLACEHOLDER"
+PLAYER_MOVEMENT_SPEED = 30
+GRAVITY = -1
+
+class GameView(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class to set up the window
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, fullscreen=True)
+        self.beer_texture = arcade.load_texture("D:\PLACEHOLDER\Assets\KaydenGameBarBeerAnimation.gif")
+        self.beer_sprite = arcade.Sprite(self.beer_texture)
+        self.beer_sprite.center_x = WINDOW_WIDTH/2
+        self.beer_sprite.center_y = WINDOW_HEIGHT/2
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.beer_sprite)
+
+
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+
+        pass
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # The clear method should always be called at the start of on_draw.
+        # It clears the whole screen to whatever the background color is
+        # set to. This ensures that you have a clean slate for drawing each
+        # frame of the game.
+        self.clear()
+
+        # Code to draw other things will go here
+        arcade.draw_sprite(self.beer_sprite)
+
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+        self.beer_sprite.change_y = GRAVITY
+    def on_key_hold(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.beer_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.beer_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.beer_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.beer_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called whenever a key is released."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.beer_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.beer_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.beer_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.beer_sprite.change_x = 0
+
+
+def main():
+    """Main function"""
+    window = GameView()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
