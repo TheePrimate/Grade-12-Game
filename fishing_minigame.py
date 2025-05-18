94b4d79283a66e5d1e9648f7ab609165ca12ab24
Pearson <pearsontht@gmail.com>
2025-05-18 18:10:13 -0400

    V.0.2.0
    1) Added library file containing imported libraries & the structure of data storage of the fish.
    
    2) Added physics engine.
    
    3) Added placeholder assets.
    
    4) Made a progress bar for fishing.
    
    5) Indicator can no longer pass boundaries.
    
    6) Prepared code for fishing mini-game visuals using placeholder assets.

--- fishing_minigame.py
+++ fishing_minigame.py
@@ -1,9 +1,5 @@
-# The function in this file will be called when the player fishes.
-
-import arcade
-
-# Constants
 from constants import *
+from library import *
 
 
 class FishingMiniGame(arcade.Window):
@@ -14,15 +10,62 @@ class FishingMiniGame(arcade.Window):
     def __init__(self):
 
         # Call the parent class to set up the window
+        self.fishing_ticks = 0
+        self.fishing_seconds = 0
+
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, fullscreen=True)
-        self.fish_texture = arcade.load_texture("assets/KaydenGameBarBeerAnimation.gif")
-        self.fish_sprite = arcade.Sprite(self.fish_texture)
-        self.fish_sprite.center_x = WINDOW_WIDTH/2
-        self.fish_sprite.center_y = WINDOW_HEIGHT/2
+        self.fishing_sprite_list = arcade.SpriteList()
+        self.wall_block = arcade.SpriteList()
+        self.progress_bar_height = 30
 
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.fish_sprite)
+        self.hook_texture = arcade.load_texture("assets/KaydenGameBarBeerAnimation.gif")
+        self.hook_sprite = arcade.Sprite(self.hook_texture)
+        self.hook_sprite.center_x = WINDOW_WIDTH/2
+        self.hook_sprite.center_y = WINDOW_HEIGHT/2
+        self.fishing_sprite_list.append(self.hook_sprite)
 
+        self.indicator_texture = arcade.load_texture("assets/KaydenGameBarDarrienAsCustomer.png")
+        self.indicator_sprite = arcade.Sprite(self.indicator_texture)
+        self.indicator_sprite.center_x = WINDOW_WIDTH/2
+        self.indicator_sprite.center_y = WINDOW_HEIGHT/3
+        self.fishing_sprite_list.append(self.indicator_sprite)
+
+        self.hook_center_texture = arcade.load_texture("assets/KaydenGameBarDarrienAsCustomer.png")
+        self.hook_center_sprite = arcade.Sprite(self.hook_center_texture, scale=0.1)
+        self.hook_center_sprite.center_x = self.hook_sprite.center_x
+        self.hook_center_sprite.center_y = self.hook_sprite.center_y
+        self.fishing_sprite_list.append(self.hook_center_sprite)
+
+        self.hooking_container_top_texture = arcade.load_texture("assets/funnypfp.png")
+        self.hooking_container_top_sprite = arcade.Sprite(self.hooking_container_top_texture, scale=0.05)
+        self.hooking_container_top_sprite.center_x = WINDOW_WIDTH/2
+        self.hooking_container_top_sprite.center_y = 100
+        self.fishing_sprite_list.append(self.hooking_container_top_sprite)
+        self.wall_block.append(self.hooking_container_top_sprite)
+
+        self.hooking_container_bot_texture = arcade.load_texture("assets/funnypfp.png")
+        self.hooking_container_bot_sprite = arcade.Sprite(self.hooking_container_bot_texture, scale=0.05)
+        self.hooking_container_bot_sprite.center_x = WINDOW_WIDTH / 2
+        self.hooking_container_bot_sprite.center_y = 800
+        self.fishing_sprite_list.append(self.hooking_container_bot_sprite)
+        self.wall_block.append(self.hooking_container_bot_sprite)
+
+        self.hooking_container_left_texture = arcade.load_texture("assets/funnypfp.png")
+        self.hooking_container_left_sprite = arcade.Sprite(self.hooking_container_left_texture, scale=0.05)
+        self.hooking_container_left_sprite.center_x = WINDOW_WIDTH / 4
+        self.hooking_container_left_sprite.center_y = WINDOW_HEIGHT / 4
+        self.fishing_sprite_list.append(self.hooking_container_left_sprite)
+
+        self.hooking_container_right_texture = arcade.load_texture("assets/funnypfp.png")
+        self.hooking_container_right_sprite = arcade.Sprite(self.hooking_container_right_texture, scale=0.05)
+        self.hooking_container_right_sprite.center_x = WINDOW_WIDTH / 4
+        self.hooking_container_right_sprite.center_y = WINDOW_HEIGHT / 4
+        self.fishing_sprite_list.append(self.hooking_container_right_sprite)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.hook_sprite, None,
+                                                             GRAVITY, None, self.wall_block)
+
+        self.collision = None
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -38,23 +81,33 @@ class FishingMiniGame(arcade.Window):
         # frame of the game.
         self.clear()
 
-        # Code to draw other things will go here
-        arcade.draw_sprite(self.fish_sprite)
+        arcade.draw_lbwh_rectangle_filled(50, 50, 50, self.progress_bar_height, (255, 0, 0))
+        arcade.draw_text(f'{self.progress_bar_height}%', 50, 50)
+        self.fishing_sprite_list.draw()
 
     def on_update(self, delta_time):
+        self.fishing_sprite_list.update()
+        self.hook_center_sprite.center_x = self.hook_sprite.center_x
+        self.hook_center_sprite.center_y = self.hook_sprite.center_y
         self.physics_engine.update()
-        self.fish_sprite.center_y += GRAVITY
+        self.collision = arcade.check_for_collision(self.hook_center_sprite, self.indicator_sprite)
+        if self.collision is True:
+            self.fishing_ticks += 1
+            if self.progress_bar_height < 100:
+                self.progress_bar_height += 2
+            if self.fishing_ticks % 30 == 0:
+                self.fishing_seconds += 1
+                print(self.fishing_seconds)
+
+        if self.progress_bar_height > 0:
+            self.progress_bar_height -= 1
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
         """
         if button == arcade.MOUSE_BUTTON_LEFT:
-            self.fish_sprite.change_y = PLAYER_MOVEMENT_SPEED
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.fish_sprite.change_y = 0
+            self.hook_sprite.change_y = PLAYER_MOVEMENT_SPEED
 
 
 def main():
