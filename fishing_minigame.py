da853255721574cba10fccecffe35888bcc3a558
Pearson <pearsontht@gmail.com>
2025-05-18 20:24:37 -0400

    V.0.3.0
    Added indicator random movement within the boundaries

--- fishing_minigame.py
+++ fishing_minigame.py
@@ -1,3 +1,5 @@
+import random
+
 from constants import *
 from library import *
 
@@ -12,6 +14,11 @@ class FishingMiniGame(arcade.Window):
         # Call the parent class to set up the window
         self.fishing_ticks = 0
         self.fishing_seconds = 0
+        self.indicator_ticks = 0
+        self.indicator_change_speed = 0
+        self.indicator_change_direction = 0
+        self.indicator_change_speed_ticks = 0
+        self.indicator_change_direction_ticks = 0
 
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, fullscreen=True)
         self.fishing_sprite_list = arcade.SpriteList()
@@ -20,14 +27,14 @@ class FishingMiniGame(arcade.Window):
 
         self.hook_texture = arcade.load_texture("assets/KaydenGameBarBeerAnimation.gif")
         self.hook_sprite = arcade.Sprite(self.hook_texture)
-        self.hook_sprite.center_x = WINDOW_WIDTH/2
-        self.hook_sprite.center_y = WINDOW_HEIGHT/2
+        self.hook_sprite.center_x = WINDOW_WIDTH / 2
+        self.hook_sprite.center_y = WINDOW_HEIGHT / 2
         self.fishing_sprite_list.append(self.hook_sprite)
 
         self.indicator_texture = arcade.load_texture("assets/KaydenGameBarDarrienAsCustomer.png")
         self.indicator_sprite = arcade.Sprite(self.indicator_texture)
-        self.indicator_sprite.center_x = WINDOW_WIDTH/2
-        self.indicator_sprite.center_y = WINDOW_HEIGHT/3
+        self.indicator_sprite.center_x = WINDOW_WIDTH / 2
+        self.indicator_sprite.center_y = WINDOW_HEIGHT / 3
         self.fishing_sprite_list.append(self.indicator_sprite)
 
         self.hook_center_texture = arcade.load_texture("assets/KaydenGameBarDarrienAsCustomer.png")
@@ -38,7 +45,7 @@ class FishingMiniGame(arcade.Window):
 
         self.hooking_container_top_texture = arcade.load_texture("assets/funnypfp.png")
         self.hooking_container_top_sprite = arcade.Sprite(self.hooking_container_top_texture, scale=0.05)
-        self.hooking_container_top_sprite.center_x = WINDOW_WIDTH/2
+        self.hooking_container_top_sprite.center_x = WINDOW_WIDTH / 2
         self.hooking_container_top_sprite.center_y = 100
         self.fishing_sprite_list.append(self.hooking_container_top_sprite)
         self.wall_block.append(self.hooking_container_top_sprite)
@@ -62,8 +69,10 @@ class FishingMiniGame(arcade.Window):
         self.hooking_container_right_sprite.center_y = WINDOW_HEIGHT / 4
         self.fishing_sprite_list.append(self.hooking_container_right_sprite)
 
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.hook_sprite, None,
-                                                             GRAVITY, None, self.wall_block)
+        self.physics_engine1 = arcade.PhysicsEnginePlatformer(self.hook_sprite, None,
+                                                              GRAVITY, None, self.wall_block)
+        self.physics_engine2 = arcade.PhysicsEnginePlatformer(self.indicator_sprite, None,
+                                                              0, None, self.wall_block)
 
         self.collision = None
 
@@ -86,21 +95,34 @@ class FishingMiniGame(arcade.Window):
         self.fishing_sprite_list.draw()
 
     def on_update(self, delta_time):
+        self.indicator_ticks += 1
         self.fishing_sprite_list.update()
         self.hook_center_sprite.center_x = self.hook_sprite.center_x
         self.hook_center_sprite.center_y = self.hook_sprite.center_y
-        self.physics_engine.update()
+        self.physics_engine1.update()
+        self.physics_engine2.update()
         self.collision = arcade.check_for_collision(self.hook_center_sprite, self.indicator_sprite)
         if self.collision is True:
             self.fishing_ticks += 1
             if self.progress_bar_height < 100:
-                self.progress_bar_height += 2
+                self.progress_bar_height += 3
             if self.fishing_ticks % 30 == 0:
                 self.fishing_seconds += 1
                 print(self.fishing_seconds)
 
         if self.progress_bar_height > 0:
             self.progress_bar_height -= 1
+
+        self.indicator_change_direction = random.randint(0, 1)
+        self.indicator_change_speed_ticks = random.randint(1, 2) * 30
+        self.indicator_change_direction_ticks = random.randint(1, 2) * 30
+
+        if self.indicator_ticks % self.indicator_change_speed_ticks == 0:
+            self.indicator_sprite.change_y = random.randint(0, 5)
+
+        if self.indicator_ticks % self.indicator_change_direction_ticks == 0:
+            if self.indicator_change_direction == 1:
+                self.indicator_sprite.change_y = -self.indicator_sprite.change_y
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
