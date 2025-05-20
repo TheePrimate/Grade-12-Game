df88c82d85d974d3e93f2e133f3381f5c67b2382
dounodaone1 <nathanyang2007@gmail.com>
2025-05-20 14:07:55 -0400

    V.0.5.0 Adding a start screen and a rules screen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # Main game loop goes here
-import arcade
-
 from constants import *
 from library import *
 
@@ -21,10 +19,12 @@ class GameView(arcade.View):
        self.label_timer_ticks = 0
        self.beer_sprite.center_x = WINDOW_WIDTH/2
        self.beer_sprite.center_y = WINDOW_HEIGHT/2
-       self.money_quota = 100 + self.day * 20
+       self.money_quota = 100 + self.day * 10
        self.physics_engine = arcade.PhysicsEngineSimple(
            self.beer_sprite)
        self.show_quota_label = False
+       self.fishing_minigame_activate = False
+
        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
 
 
@@ -41,14 +41,10 @@ class GameView(arcade.View):
        # frame of the game.
        self.clear()
        if self.show_quota_label:
-           arcade.draw_text(
-               f"Tomorrow's Quota: ${self.money_quota + 20}",
-               200, 300,
-               arcade.color.YELLOW,
-               24
-           )
+           arcade.draw_text(f"Today's Quota: ${self.money_quota + 10}",WINDOW_WIDTH/2, 300, arcade.color.YELLOW,24,align="center")
+       arcade.draw_text(f"Day: {self.day}", WINDOW_WIDTH/2, 50, arcade.color.RED, 20, align='center')
        # Code to draw other things will go here
-       arcade.draw_sprite(self.beer_sprite)
+
 
 
    def on_update(self, delta_time):
@@ -56,9 +52,8 @@ class GameView(arcade.View):
        if self.beer_sprite.center_y > 40:
            self.beer_sprite.center_y -= GRAVITY
        self.timer += 1
-       if self.timer % 1000 == 0:
+       if self.timer % 400 == 0:
            self.trigger_mob()
-
 
        if self.show_quota_label:
            self.label_timer_ticks += 1
@@ -69,43 +64,25 @@ class GameView(arcade.View):
 
    def on_key_press(self, key, modifiers):
        """Called whenever a key is pressed."""
-
-
-       if key == arcade.key.UP or key == arcade.key.W:
-           self.beer_sprite.change_y = PLAYER_MOVEMENT_SPEED
-       elif key == arcade.key.DOWN or key == arcade.key.S:
-           self.beer_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-       elif key == arcade.key.LEFT or key == arcade.key.A:
-           self.beer_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-       elif key == arcade.key.RIGHT or key == arcade.key.D:
-           self.beer_sprite.change_x = PLAYER_MOVEMENT_SPEED
-       elif key == arcade.key.SPACE:
+       if key == arcade.key.SPACE:
            print(self.timer)
            print(self.day)
            print(self.show_quota_label)
            print(self.money_quota)
 
-
    def on_key_release(self, key, modifiers):
        """Called whenever a key is released."""
+       pass
 
-
-       if key == arcade.key.UP or key == arcade.key.W:
-           self.beer_sprite.change_y = 0
-       elif key == arcade.key.DOWN or key == arcade.key.S:
-           self.beer_sprite.change_y = 0
-       elif key == arcade.key.LEFT or key == arcade.key.A:
-           self.beer_sprite.change_x = 0
-       elif key == arcade.key.RIGHT or key == arcade.key.D:
-           self.beer_sprite.change_x = 0
-
+   def on_mouse_press(self, x, y, button, modifiers):
+       self.fishing_minigame_activate = True
 
    def trigger_mob(self):
        print('The baddies are here')
        self.day += 1
-       self.money_quota = 100 + (self.day - 1) * 20
        self.show_quota_label = True
        self.timer = 0
+
 
 
 class GameStartView(arcade.View):
@@ -132,6 +109,7 @@ class GameStartView(arcade.View):
         if key == arcade.key.ESCAPE:
             game_view = RulesView()
             self.window.show_view(game_view)
+
 
 class RulesView(arcade.View):
     def __init__(self):
