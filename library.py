87d964035cfa5c56f66911a36c1ef6e1e4b98b46
Pearson <pearsontht@gmail.com>
2025-05-29 10:50:49 -0400

    V.0.7.5
    1) Small renaming changes.
    2) Added names of assets into fish dictionary.

--- library.py
+++ library.py
@@ -6,12 +6,12 @@ import random
 fish_data = {
     "Rainbow Trout": [39, 9, 1, ""],
     "Dumbo Octopus": [99999, 1, 10, ""],
-    "Codfish": [270, 8, 2, ""],
-    "Ancient Mariner's Albatross": [553000, 1, 10, ""],
-    "Bluefin Tuna": [7350, 4, 10, ""],
+    "Codfish": [270, 8, 2, "codfish.png"],
+    "Ancient Mariner's Albatross": [553000, 1, 10, "albatross.png"],
+    "Bluefin Tuna": [7350, 4, 10, "bluefin_tuna"],
     "Immortal Jellyfish": [0.3, 2, ""],
-    "Snoek": [1500, 7, 3, ""],
-    "Marlin": [4800, 5, 9, ""],
+    "Snoek": [1500, 7, 3, "snoek.png"],
+    "Marlin": [4800, 5, 9, "marlin.png"],
     "Northern Pike": [-200, 7, 4, ""],
-    "Sockeye Salmon": [96, 6, 5, ""]
+    "Sockeye Salmon": [96, 6, 5, "sockeye.png"]
 }
